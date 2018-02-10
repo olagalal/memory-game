0a60957ac741d2436f0ac53199f1aa72905c1759
Ola Glal <olaglal97@hotmail.com>
2018-02-10 19:08:10 +0200

    init js file

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
-/*
- * Create a list that holds all of your cards
- */
+// Cards Array
+const cards = ["diamond", "diamond", "paper-plane", "paper-plane", "anchor", "anchor", "bolt", "bolt", "cube", "cube", "leaf", "leaf", "bicycle", "bicycle", "bomb", "bomb"];
 
+// Variables
+let openCards = [];
+let moves = 0;
+let stars = 3;
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+// Variables for Timer
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+//Shuffle Array
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -25,14 +26,28 @@ function shuffle(array) {
     return array;
 }
 
+// Shuffle Cards
+function shuffleCards() {
+    let newSetOfCards = shuffle(cards);
+    openCards = [];
+    const deck = $('.deck').empty();
+    for (let card of newSetOfCards) {
+        let cardContainer = $('<li class="card"><i class="fa" aria-hidden="true"></i></li>');
+        deck.append(cardContainer);
+        let iconClass = "fa-" + card;
+        cardContainer.find('.fa').addClass(iconClass);
+    }
+}
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+// Click a card on the deck to show cards and check the matching of cards
+$('.deck').on('click', '.card', function (event) {
+    // open the clicked card
+    let clickedCard = $(event.target);
+
+    showCard(clickedCard);
+
+    setTimeout(function () {
+        checkCards(clickedCard);
+    }, 600);
+
+});
